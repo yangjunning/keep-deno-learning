f58b14ad17198ab75dcdf15e6407c950120cca69
杨俊宁 <youngjuning@aliyun.com>
2020-08-06 00:03:07 +0800

    first commit

--- denv/index.ts
+++ denv/index.ts
@@ -1,3 +1,4 @@
-import "https://deno.land/x/denv/mod.ts";
+import { load } from "https://deno.land/x/denv/mod.ts";
+await load();
 console.log(Deno.env.get("HOME")); // e.g. outputs "/home/alice"
 console.log(Deno.env.get("APP_PORT")); // outputs "Undefined"
